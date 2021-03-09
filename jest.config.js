5cb0c4d4fa270c27b54588e34349b6e68362cded
David <4661784+retyui@users.noreply.github.com>
2021-03-10 00:08:35 +0200

    Improve code quality and CI checks

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"]
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['esm', 'cmj', 'ts.test.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
