7c4d8d732ba07804b7cc091ee7d41262c69b54c6
Brad Cerasani <bradcerasani@gmail.com>
2022-04-03 16:49:03 -0500

    Add isSsr check to utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 import { UpdateAction } from './PinchZoom/types';
 
+const isSsr = typeof window === 'undefined';
+
 export const isTouch = () =>
-  'ontouchstart' in window || navigator.maxTouchPoints > 0;
+  !isSsr && ('ontouchstart' in window || navigator.maxTouchPoints > 0);
 
 export const make2dTransformValue = ({ x, y, scale }: UpdateAction) =>
   `scale(${scale}) translate(${x}px, ${y}px)`;
@@ -10,7 +12,7 @@ export const make3dTransformValue = ({ x, y, scale }: UpdateAction) =>
   `scale3d(${scale},${scale}, 1) translate3d(${x}px, ${y}px, 0)`;
 
 export const hasTranslate3DSupport = () => {
-  const css = window.CSS;
+  const css = !isSsr && window.CSS;
 
   return css && css.supports && css.supports('transform', 'translate3d(0,0,0)');
 };
